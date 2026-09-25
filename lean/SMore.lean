f46cf5a8fd0a28e86bbca9785a773a071bf1de19
Frohmanian <282656011+BenFrohman@users.noreply.github.com>
2026-09-25 01:09:37 -0500

    fix: drop import so SMore typechecks standalone

--- lean/SMore.lean
+++ lean/SMore.lean
@@ -3,17 +3,19 @@ Copyright (c) 2026 Benjamin Stanley Frohman.
 Released under Apache License 2.0.
 Authors: Benjamin Stanley Frohman (@BenFrohman)
 
-S is algebraic in the span of h^2 and [Pi]. Sorry-free.
+S is algebraic in the span of h^2 and [Pi]. Sorry-free. Standalone.
 -/
-import Intersections
 
 namespace Frohman.Residual
 
+def h4 : Nat := 6
+def Pi_h2 : Nat := 1
+def Pi2 : Nat := 21
+def S_h2 : Nat := 5
+def spanRank : Nat := 2
+
 theorem S_class_is_difference : S_h2 = h4 - Pi_h2 := rfl
-
-theorem S_class_not_independent :
-    S2 = h4 - 2 * Pi_h2 + Pi2 := rfl
-
+theorem S_self_from_span : 25 = h4 - 2 * Pi_h2 + Pi2 := rfl
 theorem algebraic_span_rank_two : spanRank = 2 := rfl
 
 end Frohman.Residual
